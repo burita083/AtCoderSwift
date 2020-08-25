32f1230bd46b33293bf6dff44383a9851339eda9
burita083 <s.naka0523@gmail.com>
2020-08-25 12:07:40 +0900

    daily commit

--- AtCoderSwift/main.swift
+++ AtCoderSwift/main.swift
@@ -7,6 +7,24 @@
 //
 
 import Foundation
+func readInt() -> Int {
+    return Int(readLine()!)!
+}
+ 
+func readInts() -> [Int] {
+    return readLine()!.split(separator: " ").map { Int($0)! }
+}
+ 
+func readTwoInts() -> (a: Int, b: Int) {
+    let ints = readLine()!.split(separator: " ").map { Int($0)! }
+    return (a: ints[0], b: ints[1])
+}
 
-print("Hello, World!")
 
+func abc080_b() {
+    let N = readLine()!
+    let sum = N.map { Int(String($0))! }.reduce(0, +)
+    print(Int(N)! % sum == 0 ? "Yes" : "No")
+}
+
+abc080_b()
---
+++ auto-commit.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+path="/Users/nakazy/Git/AtCoderSwift"
+cd $path
+git add .
+git commit -m "daily commit"
+git push
